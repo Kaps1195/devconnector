bd89e180722ac3c1179a7408e82d1776d2cb462b
kapil <kapilramnani@plexusmd.com>
2020-09-07 12:13:44 +0530

    setAuthToken as common header + auth reducer + alert case fix + Navbar redux for login/logout + Register user API connect + Login user API connect

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useEffect } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
@@ -6,11 +6,24 @@ import Register from './components/auth/Register';
 import Login from './components/auth/Login';
 import Alert from './components/layout/Alert';
 import './App.css';
+import setAuthToken from './utils/setAuthToken'
+import { loadUser } from './actions/auth'
 // Redux
 import { Provider } from 'react-redux';
 import store from './store';
 
-const App = () => (
+
+if(localStorage.token) {
+  setAuthToken(localStorage.token)
+}
+
+const App = () => {
+  
+  useEffect(() => {
+    store.dispatch(loadUser(), []);
+  });
+
+  return (
   <Provider store = {store}>
     <Router>
       <Fragment>
@@ -26,7 +39,6 @@ const App = () => (
       </Fragment>
     </Router>
   </Provider>
-
-)
+)};
 
 export default App;
